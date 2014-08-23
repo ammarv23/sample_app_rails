8f6422d889f4185295546d11f00de8b3737db167
ammarv23 <ammarv23@gmail.com>
2014-08-22 18:22:09 -0700

    Added a couple more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe User, :type => :model do
 
 	describe "when email format is invalid" do
 		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foobar@example..com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
@@ -92,5 +92,14 @@ RSpec.describe User, :type => :model do
 		it {should be_invalid}
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) {"FooBAr@ExAmplE.cOm"}
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
 
 end
